257193470b60463c1693a701511ebc9c547544dc
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2020-11-18 22:40:36 +0100

    added ImGUI event

--- ClemEngine/src/ClemEngine/ImGui/ImGuiLayer.h
+++ ClemEngine/src/ClemEngine/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "ClemEngine/Layer.h"
 
+#include "ClemEngine/Events/ApplicationEvent.h"
+#include "ClemEngine/Events/KeyEvent.h"
+#include "ClemEngine/Events/MouseEvent.h"
+
 namespace ClemEngine
 {
 	class CLEMENGINE_API ImGuiLayer : public Layer
@@ -14,6 +18,16 @@ namespace ClemEngine
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrollEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizedEvent(WindowResizeEvent& e);
 
 	private:
 		float m_Time = 0.0f;
