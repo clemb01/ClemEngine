f0a858a28ae100c379fea93be78b6eadb2ae1ae6
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2020-11-22 22:45:19 +0100

    RendererAPI abstraction

--- ClemEngine/src/ClemEngine/Application.h
+++ ClemEngine/src/ClemEngine/Application.h
@@ -10,6 +10,7 @@
 #include "ClemEngine/ImGui/ImGuiLayer.h"
 
 #include "ClemEngine/Renderer/Shader.h"
+#include "ClemEngine/Renderer/Buffer.h"
 
 namespace ClemEngine 
 {
@@ -36,8 +37,10 @@ namespace ClemEngine
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 
 		static Application* s_Instance;
 	};
---
+++ ClemEngine/src/ClemEngine/Renderer/Renderer.cpp
@@ -0,0 +1,7 @@
+#include "cepch.h"
+#include "Renderer.h"
+
+namespace ClemEngine
+{
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
