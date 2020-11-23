b218754087b750d227835ff457aa70d52932d94f
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2020-11-23 23:24:34 +0100

    Added buffer layout

--- ClemEngine/src/ClemEngine/Application.cpp
+++ ClemEngine/src/ClemEngine/Application.cpp
@@ -13,6 +13,28 @@ namespace ClemEngine
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case ClemEngine::ShaderDataType::None:		return GL_FLOAT;
+			case ClemEngine::ShaderDataType::Float:		return GL_FLOAT;
+			case ClemEngine::ShaderDataType::Float2:	return GL_FLOAT;
+			case ClemEngine::ShaderDataType::Float3:	return GL_FLOAT;
+			case ClemEngine::ShaderDataType::Float4:	return GL_FLOAT;
+			case ClemEngine::ShaderDataType::Mat3:		return GL_FLOAT;
+			case ClemEngine::ShaderDataType::Mat4:		return GL_FLOAT;
+			case ClemEngine::ShaderDataType::Int:		return GL_INT;
+			case ClemEngine::ShaderDataType::Int2:		return GL_INT;
+			case ClemEngine::ShaderDataType::Int3:		return GL_INT;
+			case ClemEngine::ShaderDataType::Int4:		return GL_INT;
+			case ClemEngine::ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		CE_CORE_ASSERT(false, "Unknown ShaderDataType !");
+		return 0;
+	}
+
 	Application::Application() 
 	{
 		CE_CORE_ASSERT(!s_Instance, "Application already exist !");
@@ -27,30 +49,58 @@ namespace ClemEngine
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float vertices[4 * 7] = {
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+			-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+			0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+			0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+			{ ShaderDataType::Float3, "a_Position" },
+			{ ShaderDataType::Float4, "a_Color" },
+			};
 
-		uint32_t indices[3] = { 0, 1, 2 };
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, 
+				element.GetComponentCount(), 
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
+		uint32_t indices[6] = 
+		{ 
+			0, 1, 2,
+			0, 2, 3
+		};
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 
 		std::string vertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -61,10 +111,12 @@ namespace ClemEngine
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color; 
 			}
 		)";
 
