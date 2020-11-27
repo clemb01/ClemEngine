7d03366e345d1aac52215882236fee6a24c60a57
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2020-11-27 18:26:22 +0100

    Shader abstraction

--- Minecraft/src/MinecraftApp.cpp
+++ Minecraft/src/MinecraftApp.cpp
@@ -1,14 +1,18 @@
 #include <ClemEngine.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public ClemEngine::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_squarePosition(0.0f)
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
 	{
 		m_VertexArray.reset(ClemEngine::VertexArray::Create());
 
@@ -94,9 +98,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new ClemEngine::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(ClemEngine::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -113,20 +117,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc2 = R"(
+		std::string flatColorShaderFragmentSrc2 = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new ClemEngine::Shader(blueShaderVertexSrc, blueShaderFragmentSrc2));
+		m_FlatColorShader.reset(ClemEngine::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc2));
 	}
 
 	void OnUpdate(ClemEngine::Timestep ts) override
@@ -156,13 +162,21 @@ public:
 
 		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f); 
+
+
+		std::dynamic_pointer_cast<ClemEngine::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<ClemEngine::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				ClemEngine::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+
+				ClemEngine::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -173,7 +187,9 @@ public:
 	
 	virtual void OnImGuiRender() override
 	{
-
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(ClemEngine::Event& event) override
@@ -185,7 +201,7 @@ private :
 	std::shared_ptr<ClemEngine::Shader> m_Shader;
 	std::shared_ptr<ClemEngine::VertexArray> m_VertexArray;
 
-	std::shared_ptr<ClemEngine::Shader> m_BlueShader;
+	std::shared_ptr<ClemEngine::Shader> m_FlatColorShader;
 	std::shared_ptr<ClemEngine::VertexArray> m_SquareVA;
 
 	ClemEngine::OrthographicCamera m_Camera;
@@ -195,8 +211,7 @@ private :
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 90.0f;
 
-	glm::vec3 m_squarePosition;
-	float m_SquareMoveSpeed = 2.0f;
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class MinecraftApp : public ClemEngine::Application
