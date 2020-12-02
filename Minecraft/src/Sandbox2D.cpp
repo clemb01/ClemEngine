de1a7197d6c04c805f173077fe6b60b02ff47d15
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2020-12-02 17:56:18 +0100

    shader abstraction + transform

--- Minecraft/src/Sandbox2D.cpp
+++ Minecraft/src/Sandbox2D.cpp
@@ -5,8 +5,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "Platform/OpenGL/OpenGLShader.h"
-
 Sandbox2D::Sandbox2D()
 	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f)
 {
@@ -30,14 +28,10 @@ void Sandbox2D::OnUpdate(ClemEngine::Timestep ts)
 
 	ClemEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-	ClemEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	ClemEngine::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	ClemEngine::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 1.0f, 1.0f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 
 	ClemEngine::Renderer2D::EndScene();
-
-	//std::dynamic_pointer_cast<ClemEngine::OpenGLShader>(m_FlatColorShader)->Bind();
-	//std::dynamic_pointer_cast<ClemEngine::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
-
-	//ClemEngine::Renderer::Submit(m_FlatColorShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 }
 
 void Sandbox2D::OnImGuiRender()
