c023546b861fb82865edd48330c2a3b9fe6a6908
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2020-12-21 18:49:00 +0100

    Refactored input situation

--- ClemEngine/src/Platform/Windows/WindowsInput.cpp
+++ ClemEngine/src/Platform/Windows/WindowsInput.cpp
@@ -1,12 +1,12 @@
 #include "cepch.h"
-#include "WindowsInput.h"
+#include "ClemEngine/Core/Input.h"
 
 #include "ClemEngine/Core/Application.h"
 #include <GLFW/glfw3.h>
 
 namespace ClemEngine
 {
-	bool WindowsInput::IsKeyPressedImpl(KeyCode keycode)
+	bool Input::IsKeyPressed(KeyCode keycode)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, static_cast<int32_t>(keycode));
@@ -14,7 +14,7 @@ namespace ClemEngine
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(MouseCode button)
+	bool Input::IsMouseButtonPressed(MouseCode button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
@@ -22,7 +22,7 @@ namespace ClemEngine
 		return state == GLFW_PRESS;
 	}
 
-	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	std::pair<float, float> Input::GetMousePosition()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xpos, ypos;
@@ -31,16 +31,16 @@ namespace ClemEngine
 		return std::pair<float, float>((float)xpos, (float)ypos);
 	}
 
-	float WindowsInput::GetMouseXImpl()
+	float Input::GetMouseX()
 	{
-		auto [x, y] = GetMousePositionImpl();
+		auto [x, y] = GetMousePosition();
 
 		return x;
 	}
 
-	float WindowsInput::GetMouseYImpl()
+	float Input::GetMouseY()
 	{
-		auto [x, y] = GetMousePositionImpl();
+		auto [x, y] = GetMousePosition();
 
 		return y;
 	}
