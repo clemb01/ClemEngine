3bb6f56cf2a16b578df8f824561201133f72df41
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2021-01-03 12:38:47 +0100

    Scene Camera

--- ClemEngine/src/ClemEngine/Scene/Components.h
+++ ClemEngine/src/ClemEngine/Scene/Components.h
@@ -2,7 +2,7 @@
 
 #include <glm/glm.hpp>
 
-#include "ClemEngine/Renderer/Camera.h"
+#include "SceneCamera.h"
 
 namespace ClemEngine
 {
@@ -41,12 +41,11 @@ namespace ClemEngine
 
 	struct CameraComponent
 	{
-		ClemEngine::Camera Camera;
+		SceneCamera Camera;
 		bool Primary = true;
+		bool FixedAspectRatio = false;
 
 		CameraComponent() = default;
 		CameraComponent(const CameraComponent&) = default;
-		CameraComponent(const glm::mat4& projection)
-			: Camera(projection) {}
 	};
 }
