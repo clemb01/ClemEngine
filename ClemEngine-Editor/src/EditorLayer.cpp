c8c7e96cd86960a68bee542dfc92974f991dda01
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2021-01-05 18:17:51 +0100

    Proper scale rotation and translation

--- ClemEngine-Editor/src/EditorLayer.cpp
+++ ClemEngine-Editor/src/EditorLayer.cpp
@@ -45,8 +45,8 @@ namespace ClemEngine
 		public:
 			void OnCreate() 
 			{ 
-				auto& transform = GetComponent<TransformComponent>().Transform;
-				transform[3][0] = rand() % 10 - 5.0f;
+				auto& translation = GetComponent<TransformComponent>().Translation;
+				translation.x = rand() % 10 - 5.0f;
 			}
 
 			void OnDestroy() 
@@ -55,17 +55,17 @@ namespace ClemEngine
 
 			void OnUpdate(Timestep ts) 
 			{
-				auto& transform = GetComponent<TransformComponent>().Transform;
+				auto& translation = GetComponent<TransformComponent>().Translation;
 				float speed = 5.0f;
 
 				if (Input::IsKeyPressed(Key::Q))
-					transform[3][0] -= speed * ts;
+					translation.x -= speed * ts;
 				if (Input::IsKeyPressed(Key::D))
-					transform[3][0] += speed * ts;
+					translation.x += speed * ts;
 				if (Input::IsKeyPressed(Key::Z))
-					transform[3][1] += speed * ts;
+					translation.y += speed * ts;
 				if (Input::IsKeyPressed(Key::S))
-					transform[3][1] -= speed * ts;
+					translation.y -= speed * ts;
 			}
 		};
 
