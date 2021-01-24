9ad22ae51bb19ee2f78916e57d7311ad5efcd808
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2021-01-24 15:19:38 +0100

    Added multi render target

--- ClemEngine-Editor/src/EditorLayer.cpp
+++ ClemEngine-Editor/src/EditorLayer.cpp
@@ -27,6 +27,7 @@ namespace ClemEngine
 		m_CheckerboardTexture = Texture2D::Create("assets/textures/Checkerboard.png");
 
 		FramebufferSpecification fbSpec;
+		fbSpec.Attachments = { FramebufferTextureFormat::RGBA8, FramebufferTextureFormat::Depth };
 		fbSpec.Width = 1280;
 		fbSpec.Height = 720;
 		m_Framebuffer = Framebuffer::Create(fbSpec);
